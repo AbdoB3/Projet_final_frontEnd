7eb745beafb8f67e2cfda37442eee0679bf82b27
maryam labyed <maryamlabyed4@gmail.com>
2024-05-02 11:22:10 +0100

    add useeffect to home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Hero from './Hero'
-import  Raiting from './Rating';
+import React, { useState, useEffect } from 'react';
+import Hero from './Hero';
+import Raiting from './Rating';
 import Faq from './Faq';
 import Workprocess from './Hiw';
 import About from './About';
@@ -8,17 +8,38 @@ import Services from './Services';
 import Contact from './Contact';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import axios from 'axios';
+
 export default function Home() {
+  // State to hold the data fetched from the server
+  const [consultationData, setConsultationData] = useState([]);
+
+  useEffect(() => {
+    // Function to fetch data from the server
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3002/consultation/');
+        setConsultationData(response.data);
+        console.log('Fetched consultation data:', response.data);  g
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    // Call the fetch data function when the component mounts
+    fetchData();
+  }, []); // Empty dependency array ensures the effect runs only once on mount
+
   return (
     <div id='home'>
-        <Navbar /> 
+      <Navbar /> 
       <Hero/>
-       <About/>
+      <About/>
       <Services/>
       <Faq/>
       <Raiting/>
       <Contact/>
       <Footer /> 
     </div>
-  )
+  );
 }
